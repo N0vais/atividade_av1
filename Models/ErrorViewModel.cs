dbb616ef7cf33f95ed5c1589048b151ca55a8b20
Daniel <soldan1@uni9.edu.br.com>
2022-11-24 00:28:32 -0300

    primeiro commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace atividade_av1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
